3a53bb7075fefc1bf702ce8740b40fa36f2f0bda
SultanAssel <60469894+SultanAssel@users.noreply.github.com>
2020-09-18 18:22:12 +0600

    initial commit

---
+++ app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchboxComponent } from './searchbox/searchbox.component';
+import { ListComponent } from './list/list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchboxComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
